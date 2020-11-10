031ccaff1c364a95d3d31da5f8aebc63c5856ec2
puxiao <yangpuxiao@gmail.com>
2020-11-11 01:57:37 +0800

    Create debounce.js

---
+++ fragment/debounce.js
@@ -0,0 +1,12 @@
+//防抖：若干时间内只触发一次回调函数，若在等待过程中再次执行则重新计时
+//防抖的重点在于清零
+
+function debounce(fun,wait){
+  let timer 
+  return (...args) => {
+    clearTimeout(time)
+    timer = setTimeout(() => {
+      fun(...args)
+    })
+  }
+}
