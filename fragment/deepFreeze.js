daa2e97ac90137af1ef2e24b929bb691254aeacc
puxiao <yangpuxiao@gmail.com>
2020-11-20 14:28:11 +0800

    Create deepFreeze.js

---
+++ fragment/deepFreeze.js
@@ -0,0 +1,20 @@
+//'use strict'
+
+//默认 Object.freeze() 只能冻结第1层属性
+//通过以下递归函数，可实现深层冻结
+
+function deepFreeze(obj){
+
+    for( let key in obj){
+        console.log(key)
+        if(obj[key] !==null && typeof obj[key] === 'object'){
+            deepFreeze(obj[key])
+        }
+    }
+
+    return Object.freeze(obj)
+}
+
+const me = deepFreeze({age:18,do:{react:'React'}})
+me.do.react = 'Taro' //在严格模式下，会报 TypeError 错误
+console.log(me)
