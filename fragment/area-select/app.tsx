b5b015f41a4b66756ad3bfe460d9251c46dad7a0
puxiao <yangpuxiao@gmail.com>
2020-12-11 15:16:04 +0800

    Create app.tsx

---
+++ fragment/area-select/app.tsx
@@ -0,0 +1,11 @@
+import TestSelect from '@/components/test-select';
+
+import './App.scss'
+
+const App = () => {
+  return (
+    <TestSelect country={1} province={2} city={1} district={1} />
+  )
+}
+
+export default App;
