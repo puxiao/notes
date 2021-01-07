baa48ff809d5d63199b06b4e257c51dbec626275
puxiao <yangpuxiao@gmail.com>
2021-01-07 18:11:20 +0800

    Create index.tsx

---
+++ fragment/hexagon/index.tsx
@@ -0,0 +1,12 @@
+
+import './index.scss'
+
+const HexagonFrames = () => {
+    return (
+        <div className='hexagon'>
+            <div className='box'></div>
+        </div>
+    )
+}
+
+export default HexagonFrames
