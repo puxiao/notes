da96eb56d601e402e79cba9debb6f17e22c62839
puxiao <yangpuxiao@gmail.com>
2021-01-10 02:04:58 +0800

    Update index.tsx

--- fragment/hexagon/index.tsx
+++ fragment/hexagon/index.tsx
@@ -4,7 +4,18 @@ import './index.scss'
 const HexagonFrames = () => {
     return (
         <div className='hexagon'>
-            <div className='box'></div>
+            <div className='box'>
+                <div className='box-line top-line'></div>
+                <div className='box-line bottom-line'></div>
+            </div>
+            <div className='box right-box'>
+                <div className='box-line left-top-line'></div>
+                <div className='box-line right-bottom-line'></div>
+            </div>
+            <div className='box left-box'>
+                <div className='box-line right-top-line'></div>
+                <div className='box-line left-bottom-line'></div>
+            </div>
         </div>
     )
 }
