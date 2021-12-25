18effe3b210f33350c8304ee5cce07a39f735eb8
puxiao <yangpuxiao@gmail.com>
2021-12-25 16:03:28 +0800

    使用 JSON.parse() 函数时额外根据 Object 属性名进行排序

---
+++ fragment/JSONParseSort.js
@@ -0,0 +1,19 @@
+const currentParse = (str) => {
+    return JSON.parse(str, (key, value) => { 
+        if(key === '') return value
+        if( typeof value === 'object'){
+            return Object.keys(value).sort().reduce((result, key) =>{
+                result[key] = value[key]
+                return result
+            }, {})
+        }
+        return value
+    })
+}
+
+let strJson1='[{"stime":"12:00","endtime":"18:00","afee":18.0000},{"endtime":"12:00","stime":"00:00","afee":12.0000}]'
+let strJson2='[{"endtime":"18:00","stime":"12:00","afee":18.0000},{"endtime":"12:00","stime":"00:00","afee":12.0000}]'
+
+strJson1 = JSON.stringify(currentParse(strJson1))
+strJson2 = JSON.stringify(currentParse(strJson2))
+console.log(strJson1 === strJson2)
