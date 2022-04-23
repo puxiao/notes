f9428790ba7b87c8bcf02d22edc3bba40b467924
puxiao <yangpuxiao@gmail.com>
2022-04-23 18:54:23 +0800

    Update JSONParseSort.js

--- fragment/JSONParseSort.js
+++ fragment/JSONParseSort.js
@@ -17,3 +17,9 @@ let strJson2='[{"endtime":"18:00","stime":"12:00","afee":18.0000},{"endtime":"12
 strJson1 = JSON.stringify(currentParse(strJson1))
 strJson2 = JSON.stringify(currentParse(strJson2))
 console.log(strJson1 === strJson2)
+
+
+
+//对 JSON.stringify() 输出结果按照 4 个空格为规范进行格式缩进
+const jsonStr = JSON.stringify({a:0}, null, 4)
+console.log(jsonStr) //'{\n    "a": 4\n}'
