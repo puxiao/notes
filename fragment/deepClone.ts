6100c37e98338f954a88cc86c1b66aec5fc2668f
puxiao <yangpuxiao@gmail.com>
2023-02-11 09:10:03 +0800

    Create deepClone.ts

---
+++ fragment/deepClone.ts
@@ -0,0 +1,16 @@
+export const deepClone = (obj: any) => {
+    if (obj === null) return null;
+    let clone = { ...obj };
+    Object.keys(clone).forEach(
+        (key) =>
+        (clone[key] =
+            typeof obj[key] === "object" ? deepClone(obj[key]) : obj[key])
+    );
+    return Array.isArray(obj) && obj.length
+        ? (clone.length = obj.length) && Array.from(clone)
+        : Array.isArray(obj)
+            ? Array.from(obj)
+            : clone;
+}
+
+export default deepClone
